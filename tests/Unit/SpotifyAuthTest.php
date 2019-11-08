23b0a5ec013762f3eea72be06deb6e21f24389b0
Anton <andreyploskov@gmail.com>
2019-11-08 15:20:06 +0000

    Empty Spotify authorization tests: unit for basic token retrieval, and feature for acessing the token

---
+++ tests/Unit/SpotifyAuthTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SpotifyAuthTest extends TestCase
+{
+    /**
+    * The Secret Key should be loaded successfully from a safe location.
+    */
+    public function loadSecretKeyTest() {
+
+    }
+
+    /**
+     * Authorization header string should be generated correctly.
+     */
+    public function generateAuthHeaderTest()
+    {
+
+    }
+
+    /**
+    * Access token should be retrieved successfully from Spotify API.
+    */
+    public function retrieveAccessTokenTest() {
+
+    }
+}
