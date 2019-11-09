ddae029063b37611e511c10377d70bfded67a83c
Anton <andreyploskov@gmail.com>
2019-11-09 20:05:10 +0000

    Added empty class to test retrieving Spotify secred and client id

---
+++ app/RemoteServices/Spotify.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\RemoteServices;
+
+class Spotify {
+    public static function getSecretKey() {
+        return null;
+    }
+
+    public static function getClientId() {
+        return null;
+    }
+}
