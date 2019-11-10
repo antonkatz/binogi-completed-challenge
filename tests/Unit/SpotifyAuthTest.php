75914b301f61cda4eaa2a636d2d3bec2283b6e84
Anton <andreyploskov@gmail.com>
2019-11-10 03:28:20 +0000

    Tests for retrieving Spotify's access token are complete. This is not a clean commit, including changes to comments, class names, and addition of tests

--- tests/Unit/SpotifyAuthTest.php
+++ tests/Unit/SpotifyAuthTest.php
@@ -5,16 +5,25 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Str;
-use App\RemoteServices\Spotify;
-use PHPOption\{Some};
+use App\RemoteServices\SpotifyAuth;
+use PHPOption\{Some, None};
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+
+
+/**
+* Unit tests of functions needed to retrieve the access token from Spotify.
+*/
 class SpotifyAuthTest extends TestCase
 {
     /**
     * The Secret Key should be loaded successfully from a safe location.
     */
     public function test_loadSecretKey() {
-        $key = Spotify::getSecretKey();
+        $key = SpotifyAuth::getSecretKey();
         $this->assertInstanceOf(Some::class, $key);
         $this->assertTrue(!empty($key->get()));
     }
@@ -24,7 +33,7 @@ class SpotifyAuthTest extends TestCase
     */
     public function test_getClientId() {
         // a shorthad approach: get of an empty Option will throw an error
-        $this->assertTrue(!empty(Spotify::getClientId()->get()));
+        $this->assertTrue(!empty(SpotifyAuth::getClientId()->get()));
     }
 
     /**
@@ -33,7 +42,7 @@ class SpotifyAuthTest extends TestCase
      */
     public function test_generateAuthHeader()
     {
-        $h = Spotify::generateAuthHeader();
+        $h = SpotifyAuth::generateAuthHeader();
         $this->assertStringStartsWith('Basic', $h);
         $this->assertTrue(
             // 10 is an arbitrary number simply to check that string contains more than 'Basic'
@@ -45,8 +54,25 @@ class SpotifyAuthTest extends TestCase
     * Access token should be retrieved successfully from Spotify API.
     */
     public function test_retrieveAccessToken() {
-        $token = Spotify::retrieveAccessToken();
-        $this.assertNotEmpty($token->getToken());
-        $this.assertGreaterThan(0, $token->getLifespan());
+        $token = SpotifyAuth::retrieveAccessToken()->get();
+        $this->assertNotEmpty($token->access_token);
+        $this->assertGreaterThan(0, $token->expires_in);
+    }
+
+    /**
+    * Access token retrieval should fail and a log message should indicate so.
+    */
+    public function test_retrieveAccessTokenFail() {
+        $mock = new MockHandler([
+            new Response(400), // reason is automatically assigned from code
+        ]);
+        $handler = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handler]);
+        SpotifyAuth::setClient($client);
+
+        $token = SpotifyAuth::retrieveAccessToken();
+        $this->assertInstanceOf(None::class, $token);
+
+        // todo: test that logging indeed occurs (appears to be non-trivial)
     }
 }
