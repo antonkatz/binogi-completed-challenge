bb0de738956d40c91e5a362a18551390bca85a3b
Anton <andreyploskov@gmail.com>
2019-11-10 17:28:17 +0000

    Tests for abstract Token container that will be used to store Spotify's access token

---
+++ tests/Unit/TokenContainerTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPOption\{Some, None, Option};
+
+use App\RemoteServices\RawToken;
+use App\RemoteServices\TokenContainer;
+
+/**
+* Testing full functionality of the abstract TokenContainer, which takes care of storing and refreshing token-like objects.
+*/
+class TokenContainerTest extends TestCase
+{
+    /**
+    * The access token should be retrievable as long as it's valid.
+    */
+    public function test_retrieveValidToken() {
+        $tokenContainer = new ValidTestTokenContainer();
+        $token = $tokenContainer->getToken();
+        $this->assertNotEmpty($token->get());
+        $this->assertGreaterThan(0, $tokenContainer->getExpiresAt());
+    }
+
+    /**
+     * Access token should be refreshed if requested and is no longer valid.
+     * This operation should not have a limit of calls.
+     */
+    public function test_refreshAccessToken()
+    {
+        $lastExpiry = -1;
+        $callCount = 5;
+
+        $tokenContainer = new RefreshTestTokenContainer();
+        for ($i = 0; $i < $callCount; $i++) {
+            // each time the token will be refreshed, and so the expiry time should change
+            $this->assertNotEmpty($tokenContainer->getToken()->get());
+            $this->assertGreaterThan($lastExpiry, $tokenContainer->getExpiresAt());
+            $lastExpiry = $tokenContainer->getExpiresAt();
+        }
+        $this->assertEquals($callCount, $tokenContainer->retrieveCallCount);
+    }
+
+    /**
+    * If access token retrieval fails, the refresh function should try agian, and return an empty (None) value if it fails after max retries.
+    * There should also be a warning mail sent to notify developers of the issue.
+    */
+    public function test_refreshAccessTokenFails() {
+        $maxAttempts = 3;
+        $tokenContainer = new FailTestTokenContainer($maxAttempts);
+        $this->assertInstanceOf(None::class, $tokenContainer->getToken());
+        $this->assertEquals($maxAttempts, $tokenContainer->retrieveCallCount);
+        $this->assertTrue($tokenContainer->hasSentMail);
+    }
+}
+
+abstract class BaseTestTokenContainer extends TokenContainer {
+    public $hasSentMail = false;
+
+    protected function sendMailWarning($message) {
+        self::$hasSentMail = true;
+    }
+}
+
+class ValidTestTokenContainer extends BaseTestTokenContainer {
+    protected function retrieveToken() {
+        $t = new class implements RawToken {
+            public $access_token = 'valid_token';
+            public $expires_in = 3600;
+        };
+
+        return Option::fromValue($t);
+    }
+}
+
+class RefreshTestTokenContainer extends ValidTestTokenContainer {
+    public $retrieveCallCount = 0;
+
+    public function isTokenValid() {
+        return false;
+    }
+
+    protected function retrieveToken() {
+        $this->retrieveCallCount += 1;
+        parent::retrieveToken();
+    }
+}
+
+class FailTestTokenContainer extends BaseTestTokenContainer {
+    public $retrieveCallCount = 0;
+
+    public function __construct($maxAttempts) {
+        $this->MAX_ATTEMPTS = $maxAttempts;
+        parent::__construct();
+    }
+
+    protected function retrieveToken() {
+        $this->retrieveCallCount += 1;
+        return None::create();
+    }
+}
